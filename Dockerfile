277ebdd95ed7a1c1abee0e73d031ab3d80f58a95
AngeleBlais <94390537+AngeleBlais@users.noreply.github.com>
2024-04-14 20:43:29 +0200

    modifs

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,16 @@ FROM python:alpine3.19
 
 WORKDIR /app
 
-COPY . .
+COPY . /app
 
-RUN pip install -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
-CMD ["python", "wrapper.py"]
+EXPOSE 8081
+
+ENV UVICORN_PORT=8081
+
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8081"]
+
+
+
+
--- wrapper.py
+++ wrapper.py
@@ -1,15 +1,16 @@
 import os
 import requests
 from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException
 
 load_dotenv()
 
-def get_weather(latitude, longitude, api_key):
+app= FastAPI()
+
+def make_api_call(latitude: float, longitude: float, api_key: str):
     if not api_key:
         raise ValueError("API key not found")
-
     url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}"
-
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -19,17 +20,19 @@ def get_weather(latitude, longitude, api_key):
         print(f"Error fetching data: {e}")
         return None
 
-def main():
-    latitude= float(os.getenv("LOCATION_LATITUDE"))
-    longitude= float(os.getenv("LOCATION_LONGITUDE"))
-    api_key = os.getenv("OPENWEATHER_API_KEY")
+def extract_weather(data):
+    try:
+        weather = data['weather'][0]['main']
+        return weather
+    except KeyError as err:
+        raise HTTPException(status_code=404, detail="Weather data not found")
 
-    weather_data= get_weather(latitude, longitude, api_key)
-    if weather_data:
-        print("Weather data:")
-        print(weather_data)
-    else:
-        print("Failed to fetch weather data.")
 
-if __name__ == "__main__":
-    main()
+@app.get("/") 
+def get_weather(lat: float, lon: float):
+    api_key = os.getenv("API_KEY")
+    if not api_key:
+        raise HTTPException(status_code=500, detail="no api key configured")
+    data = make_api_call(lat, lon, api_key)
+    weather = extract_weather(data)
+    return {"latitude": lat, "longitude": lon, "weather": weather}
