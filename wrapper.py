39a1bbe167ee1801a65b508300f46c117704f25d
AngeleBlais <94390537+AngeleBlais@users.noreply.github.com>
2024-04-18 00:28:24 +0200

    fix api

--- wrapper.py
+++ wrapper.py
@@ -2,9 +2,10 @@ import os
 import requests
 from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
+from uvicorn import run
 
 load_dotenv()
-app= FastAPI()
+app = FastAPI()
 
 def make_api_call(latitude: float, longitude: float, api_key: str):
     if not api_key:
@@ -13,26 +14,27 @@ def make_api_call(latitude: float, longitude: float, api_key: str):
     try:
         response = requests.get(url)
         response.raise_for_status()
-        data= response.json()
+        data = response.json()
         return data
     except requests.exceptions.RequestException as e:
         print(f"Error fetching data: {e}")
         return None
 
 def extract_weather(data):
-    try:    
-  
-        weather = data['weather'][0]['main']
+    try:
+        weather = data['weather']
         return weather
-    except KeyError as err:
+    except (KeyError, TypeError):
         raise HTTPException(status_code=404, detail="Weather data not found")
 
-@app.get("/") 
-
+@app.get("/")
 def get_weather(lat: float, lon: float):
-    api_key = os.getenv("API_KEY")
+    api_key = os.getenv("OPENWEATHER_API_KEY")
     if not api_key:
-        raise HTTPException(status_code=500, detail="no api key configured")
-    data= make_api_call(lat, lon, api_key)
+        raise HTTPException(status_code=500, detail="No API key configured")
+    data = make_api_call(lat, lon, api_key)
     weather = extract_weather(data)
     return {"latitude": lat, "longitude": lon, "weather": weather}
+
+if __name__ == "__main__":
+    run(app, host="0.0.0.0", port=8081)
